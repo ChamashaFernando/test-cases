fb7233b95ee9327e91d240e7d27bd24b64879bf6
ChamashaFernando <chamashanethmi81@gmail.com>
2025-05-28 11:30:17 +0530

    Initial commit - add spring boot project

---
+++ src/test/java/lk/chamasha/test/cases/TestCasesApplicationTests.java
@@ -0,0 +1,13 @@
+package lk.chamasha.test.cases;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TestCasesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
